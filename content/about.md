8a6b25c7f6451f6e73eed0aaadcf930d6d893deb
guybriggs <guybriggs8278@gmail.com>
2023-10-19 17:23:07 +1100

    Starting to add CMS support

---
+++ content/about.md
@@ -0,0 +1,17 @@
+---
+title: About
+mainColour: "#007ACC"
+secondaryColour: "#EDEDED"
+textColour: "#333333"
+intro:
+  - title: Our Story
+    body: >-
+      We have been serving our customers with dedication and passion for over a
+      decade.
+
+
+      AND ALL THAT'S ABOUT TO CHANGEEEE!!!!
+  - title: Our Mission
+    body: Our mission is to provide top-notch products and services to our
+      customers, making their lives better.
+---
--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,42 @@ module.exports = {
   plugins: [
     "gatsby-plugin-postcss",
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: `content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ]
 };
