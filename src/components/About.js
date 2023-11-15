e2fca376d92f2072d9c344f320404f42c68467e1
guybriggs <guybriggs8278@gmail.com>
2023-11-15 11:01:50 +1100

    Added logo assets; About page WIP not final

--- src/components/About.js
+++ src/components/About.js
@@ -15,6 +15,35 @@ const About = ({ data }) => {
         partners
     } = data.frontmatter;
 
+    //Hardcoded
+    const logosArray = [
+        {
+            string: "The University of New South Wales",
+            url: "/src/assets/UNSW.png"
+        },
+        {
+            string: "The University of Queensland Australia",
+            url: "/src/assets/UOQ.png"
+        },
+        {
+            string: "The University of Adelaide",
+            url: "/src/assets/UOA.png"
+        },
+        {
+            string: "STEP UP",
+            url: "/src/assets/STEP-UP.png"
+        },
+    ];
+
+    function returnUrlFromString(string) {
+        for (let i = 0; i < logosArray.length; i++) {
+            if (logosArray[i].string == string) {
+                return logosArray[i].url;
+            }
+        }
+        return null;
+    }
+
     return (
     <Section props={data}>
         <h1 className='text-4xl text-center p-4'>{title}</h1>
@@ -28,11 +57,13 @@ const About = ({ data }) => {
         </div>
         <Blockquote><i>Any sufficiently advanced <b>education</b> is indistinguishable from <b>fun</b>.</i></Blockquote>
         <FullsizeVideo videoUrl={mediterraneanVideo} />
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyjcoKbQ30vh8t4aQgTIPlIuO0KDOOdZJkqPnvM-5jZrg9kub_7kssV78QQNWBr5394A&usqp=CAU'></img>
+        <img src='/src/assets/UNSW.png'></img>
         <div className='grid grid-cols-2 md:flex justify-between text-center my-16'>
         {partners.map((item, i) => (
             <div key={i}>
                 <div className='flex justify-center p-4'>
-                    <Users size={96} />
+                    <img src={returnUrlFromString(item.partner)}></img> 
                 </div>
                 <h1>{item.partner}</h1>
             </div>
