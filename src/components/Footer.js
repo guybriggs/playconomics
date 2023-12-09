6b0ea36bcbbeb7f9a9ad83f838117fb30dbc480d
Richard Antecki <richard@antecki.id.au>
2023-12-09 13:35:57 +1100

    Change terms/privacy url to match existing playconomics website links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     return (
     <footer className='absolute bottom-0 left-0 right-0 p-16 w-full text-center'>
         <ul className='flex gap-4 justify-center p-2'>
-            <li><UnderlineLink><Link to="/termsofuse">Terms of Use</Link></UnderlineLink></li>
-            <li><UnderlineLink><Link to="/privacypolicy">Privacy Policy</Link></UnderlineLink></li>
+            <li><UnderlineLink><Link to="/terms-of-use">Terms of Use</Link></UnderlineLink></li>
+            <li><UnderlineLink><Link to="/privacy-policy">Privacy Policy</Link></UnderlineLink></li>
         </ul>
         <p>© LionsHeart Studios Pty Ltd | 2023</p>
     </footer>
